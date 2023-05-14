56dfedae113211e1b3cc0a88c76ba0bf725fa209
Robert Paulsen <ozymandias.1987@protonmail.com>
2023-05-14 16:30:38 -0600

    paru2

--- tiling-wm/start-script.sh
+++ tiling-wm/start-script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cp -r ../tiling-wm '$HOME/.config' &&
-
-cp conf-files/xinitrc '$HOME' &&
+mkdir ~/.local/share/fonts &&
+cp -R IosevkaTermNerdFontComplete.ttf ~/.local/share/fonts &&
+cp -R ../tiling-wm '$HOME/.config' &&
+chmod -R +x ~/.config/bspwm &&
+chmod -R +x ~/.config/polybar/scripts &&
+sudo pacman -Syu --needed --noconfirm - < packages-repository.txt &&
+sudo fc-cache -f -v &&
 
 paru -Syu --needed --noconfirm - < '$HOME/.config/tiling-wm/package-list.txt'
